67ba78c8fa321b085fb3d4d7db9f924197aa5c96
AyomiNimasha <ayominimasha@gmail.com>
2023-07-19 20:43:38 +0530

    commit4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import { Layout } from "./components/Layout";
+import "bootstrap/dist/css/bootstrap.css"; //manually entered
+import { Navbar } from "react-bootstrap";
+import logo from "./images/logo.jpg";
 
 function App() {
   return (
     <Layout>
-      <div>userrrrrr</div>
+      <div className="App">
+        <Navbar bg="myred" variant="dark">
+          <Navbar.Brand>
+            <img src={logo} height={"30px"} width={"30px"} />
+            Logo
+          </Navbar.Brand>
+        </Navbar>
+        <div className="content">This is content</div>
+      </div>
     </Layout>
   );
 }
