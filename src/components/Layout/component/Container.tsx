3d92b917553f5f790ee4f1f4db2d5c700e00dad3
AyomiNimasha <ayominimasha@gmail.com>
2023-07-20 23:38:17 +0530

    commit06

---
+++ src/components/Layout/component/Container.tsx
@@ -0,0 +1,10 @@
+import { Button } from "react-bootstrap";
+
+export const Container = () => {
+  return (
+    <>
+      <div className="bg-red">Container</div>
+      <Button />
+    </>
+  );
+};
