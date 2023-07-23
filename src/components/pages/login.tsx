f60a07450c29b9e0bac80e5bfaa52f70d56cd10c
AyomiNimasha <ayominimasha@gmail.com>
2023-07-23 22:06:07 +0530

    com7

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import { Form } from "../Form";
 
 export const Login = () => {
   return (
     <>
       <div>
-        <h1>login</h1>
-        <Link to="/login">Login3 </Link>
+        <h1>login Page</h1>
+        <Link to="/login"></Link>
+        <Form />
       </div>
     </>
   );
