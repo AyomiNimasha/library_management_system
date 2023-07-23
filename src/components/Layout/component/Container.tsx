51ba3126a0121f385764b7501d5d341a3b518821
AyomiNimasha <ayominimasha@gmail.com>
2023-07-23 22:55:44 +0530

    com3

--- src/components/Layout/component/Container.tsx
+++ src/components/Layout/component/Container.tsx
@@ -2,11 +2,14 @@ import library from "../../../assets/library.jpg";
 export const Container = () => {
   return (
     <>
-      <div className="bg-[url('/home/nimasha/Project/library_management_system/src/assets/')]"></div>
+      <div></div>
       <>
         <div className="flex text-white justify-center"></div>
         <div>
-          <img src={library} className="w-full h-full" />
+          <img
+            src={library}
+            className="bg-cover bg-center md:h-screen w-full -mt-5"
+          />
         </div>
       </>
     </>
