1c9fa5f8a726297eda825a73e965ca6ba0e939ea
AyomiNimasha <ayominimasha@gmail.com>
2023-07-26 18:41:22 +0530

    com7

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,13 +1,42 @@
 import { Link } from "react-router-dom";
-import { Form } from "../Form";
 
 export const Login = () => {
   return (
     <>
-      <div>
-        <h1>login Page</h1>
-        <Link to="/login"></Link>
-        <Form />
+      <div className="flex bg-cyan-950 justify-center items-center h-screen">
+        <div className="bg-slate-200 bg-opacity-30 p-8 shadow-md rounded-md">
+          <h1 className="text-black text-2xl font-bold mb-4">Login</h1>
+          <Link to="/login"></Link>
+          <form className="w-64 bg-transparent">
+            <div className="mb-3">
+              <label htmlFor="email" className="block text-gray-700 mb-4">
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="w-full border rounded-md text-gray-700 py-2 px-3"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="block text-gray-700">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="border rounded-md w-full text-gray-700 py-2 px-3"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="bg-blue-500 w-full text white py-2 px-4 rounded-md hover:bg-blue-600"
+            >
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/components/pages/register.tsx
+++ src/components/pages/register.tsx
@@ -3,8 +3,50 @@ import { Link } from "react-router-dom";
 export const Register = () => {
   return (
     <>
-      <div>register</div>
-      <Link to="/">Register </Link>
+      <div className="flex bg-cyan-950 justify-center items-center h-screen">
+        <div className="bg-white p-8 shadow-md rounded-md bg-opacity-30">
+          <h1 className="text-2xl font-bold mb-4">Register</h1>
+          <Link to="/">Register </Link>
+          <form action="">
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700">
+                Name :
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full py-2 px-3 border rounded-md text-gray-700"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="w-full py-2 px-3 border rounded-md text-gray-700"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-700">
+                Password :
+              </label>
+              <input
+                type="text"
+                id="password"
+                className="w-full py-2 px-3 border rounded-md text-gray-700"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 w-full text white py-2 px-4 rounded-md hover:bg-blue-600"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
